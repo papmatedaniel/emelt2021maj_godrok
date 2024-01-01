b489172dbcde790b0afa100e33fc7bc9f5073d72
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-01-01 22:16:44 +0000

    a_hat_B_fel_meg_hatravan

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ with open("melyseg.txt") as file:
 print("1. feladat")
 print(f"A fájl adatainak száma: {len(lista)}")
 
-print("2. feladat")
+print("\n2. feladat")
 betavolsag = int(input("Adjon meg egy távolságértéket!: "))
 print(f"Ezen a helyen a felszín {lista[betavolsag]} mélyen van.")
 
-print("3. feladat")
+print("\n3. feladat")
 print(f"Az érintett terület aránya: {round(( lista.count(0) / len(lista)) * 100 , 2)} %.")
 
 with open("godrok.txt", "w", encoding="utf-8") as fil:
@@ -25,19 +25,29 @@ with open("godrok.txt", "w", encoding="utf-8") as fil:
                 szolista.append((i-len(belsolista), i))
                 belsolista = []
             
-print("5. feladat")
+print("\n5. feladat")
 with open("godrok.txt", "r", encoding="utf-8") as fill:
     print(f"Gödrök száma:", godorszam := sum(1 for i in fill.readlines()))
 
 
 godrik = [i.strip() for i in open("godrok.txt").readlines()]
-print("6. feladat")
+print("\n6. feladat")
 if lista[betavolsag] != 0:
     for index, elem in enumerate(szolista):
         if elem[0] <= betavolsag <= elem[1]:
-            meres = godrik[index+1]
+            meres = [int(i) for i in godrik[index].split()]
             print("a)")
             print(f"A gödör kezdete: {elem[0]+1} méter, a gödör vége: {elem[1]} méter.")
+            print("c)")
+            print(f"Legnagyobb mélység: {max(meres)}")
+            print("d)")
+            print(f"A térfogat {sum([i *10 for i in meres ])} m^3.")
+            print("e)")
+            print(f"A vízmennyiség {sum([(i-1) *10 for i in meres ])} m^3.")
+
+            print("b feladat, majd holnap")
+            
+
 
         
 else:
