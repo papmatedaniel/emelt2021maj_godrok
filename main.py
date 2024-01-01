6699256b900af6bbaca6a38e57dc514fc1a6aa16
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-01-01 21:28:36 +0000

    siker_megvanahatosfeladatlogikaja

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ print("2. feladat")
 betavolsag = int(input("Adjon meg egy távolságértéket!: "))
 print(f"Ezen a helyen a felszín {lista[betavolsag]} mélyen van.")
 
+print("3. feladat")
 print(f"Az érintett terület aránya: {round(( lista.count(0) / len(lista)) * 100 , 2)} %.")
 
 with open("godrok.txt", "w", encoding="utf-8") as fil:
-
+    szolista = []
     belsolista = []
     for i, e in enumerate(lista):
         if e != 0:
@@ -21,23 +22,23 @@ with open("godrok.txt", "w", encoding="utf-8") as fil:
         else:
             if len(belsolista) != 0:
                 print(*belsolista, file=fil)
+                szolista.append((i-len(belsolista), i))
                 belsolista = []
             
-
-with open("godrok.txt", "r", encoding="utf-8") as fil:
-    szotar = {}
-    szamlaloww = 0
-    for i, e in enumerate(fil.readlines()):
-        for j in e:
-            szotar[i] = 
 print("5. feladat")
 with open("godrok.txt", "r", encoding="utf-8") as fill:
+    print(f"Gödrök száma:", godorszam := sum(1 for i in fill.readlines()))
 
-    print(f"Gödrök száma:", godorszam := sum(1 for i in fill.readlines())-1)
 
+godrik = [i.strip() for i in open("godrok.txt").readlines()]
 print("6. feladat")
 if lista[betavolsag] != 0:
-    pass
+    for index, elem in enumerate(szolista):
+        if elem[0] <= betavolsag <= elem[1]:
+            meres = godrik[index+1]
+            print("a)")
+            print(f"A gödör kezdete: {elem[0]+1} méter, a gödör vége: {elem[1]} méter.")
+
         
 else:
     print("Az adott helyen nincs gödör")
